1bb075dc88ea1526d6169b090dcf78b3a93f5e06
Arthaud Proust <arthaudp33@gmail.com>
2021-09-05 13:16:49 +0200

    fix import bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const path = require('path');
-
+const TerserPlugin = require("terser-webpack-plugin");
 module.exports = {
     mode: 'production',
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+    },
     entry: './src/index.js',
+    target: 'node',
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, 'build'),
+        path: path.resolve(__dirname, 'dist'),
+        library: 'ObservedVar',
+        libraryTarget: 'umd',
+        globalObject: 'this',
+        umdNamedDefine: true
     },
 };
